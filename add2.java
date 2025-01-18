915e40c00a1e2baac5782a3d0f6a86f825f66217
VectorrX <vectorx957@gmail.com>
2025-01-18 12:37:41 +0530

    m

---
+++ add2.java
@@ -0,0 +1,14 @@
+package Calculator;
+
+import java.util.Scanner;
+
+public class add2 {
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Scanner sc = new Scanner(System.in);
+		int a = sc.nextInt();
+		int b = sc.nextInt();
+		System.out.println("Result after addition is as follows = "+ (a+b));
+	}
+
+}
